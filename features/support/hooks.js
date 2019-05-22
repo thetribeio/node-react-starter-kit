5492d9a3abd6635f463efea7a02199d878cdb419
Benoît Latinier <benoit@latinier.fr>
2019-05-22 19:19:35 +0200

    add: better information on fail

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,3 +1,19 @@
 const { After } = require('cucumber');
+const SauceLabs = require('saucelabs').default;
 
-After(function () { this.driver.quit(); });
+const { SAUCELABS_ACCESS_KEY, SAUCELABS_USER } = process.env;
+
+After(async function ({ pickle: { name }, result: { status } }) {
+    const sauceApi = new SauceLabs({ user: SAUCELABS_USER, key: SAUCELABS_ACCESS_KEY });
+    // eslint-disable-next-line no-underscore-dangle
+    const jobId = (await this.driver.getSession()).id_;
+    const passed = 'passed' === status;
+    sauceApi.updateJob(SAUCELABS_USER, jobId, {
+        name: `${this.browser} - ${name}`,
+        passed,
+    });
+    this.driver.quit();
+    if (!passed) {
+        console.log(`Failed test video: https://app.saucelabs.com/tests/${jobId}`);
+    }
+});
