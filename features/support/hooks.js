cdce39b2fe91f83f60516a80b13d51e531de289a
Benoît Latinier <benoit@latinier.fr>
2019-06-20 13:24:04 +0200

    fix: move browser init from world to Before

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,7 +1,82 @@
-const { After } = require('cucumber');
+const { After, Before } = require('cucumber');
 const SauceLabs = require('saucelabs').default;
 
-const { SAUCELABS_ACCESS_KEY, SAUCELABS_USER } = process.env;
+const { Builder } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+const chrome = require('selenium-webdriver/chrome');
+const safari = require('selenium-webdriver/safari');
+
+const { SAUCELABS_ACCESS_KEY, SAUCELABS_HOST, SAUCELABS_USER } = process.env;
+
+const capabilities = {
+    ie: {
+        browserName: 'internet explorer',
+        iedriverVersion: '3.14.0',
+        seleniumVersion: '3.14.0',
+        version: '11.285',
+    },
+    chrome: {
+        browserName: 'chrome',
+        screenResolution: '1920x1200',
+    },
+    firefox: {
+        browserName: 'firefox',
+    },
+    safari: {
+        browserName: 'safari',
+        screenResolution: '1920x1440',
+    },
+};
+
+const getBrowser = (browser) => () => new Builder()
+    .withCapabilities({
+        username: SAUCELABS_USER,
+        accessKey: SAUCELABS_ACCESS_KEY,
+        host: SAUCELABS_HOST,
+        port: 4445,
+        implicit: 5000,
+        ...capabilities[browser],
+    })
+    .usingServer(`http://${SAUCELABS_USER}:${SAUCELABS_ACCESS_KEY}@${SAUCELABS_HOST}:4445/wd/hub`)
+    .build();
+
+const getLocalBrowser = (browser, display) => {
+    let options = null;
+    let optionsFunc = null;
+
+    switch (browser) {
+        case 'chrome':
+            options = new chrome.Options();
+            optionsFunc = 'setChromeOptions';
+            break;
+        case 'safari':
+            options = new safari.Options();
+            optionsFunc = 'setSafariOptions';
+            break;
+        default:
+            options = new firefox.Options();
+            optionsFunc = 'setFirefoxOptions';
+            break;
+    }
+
+    if (!display) {
+        options.headless();
+    }
+
+    const builder = new Builder();
+
+    return builder[optionsFunc](options)
+        .forBrowser(browser)
+        .build();
+};
+
+Before(async function () {
+    if (this.local) {
+        this.driver = getLocalBrowser(this.browser, this.display);
+    } else {
+        this.driver = getBrowser(this.browser);
+    }
+});
 
 After(async function ({ pickle: { name }, result: { status } }) {
     const sauceApi = new SauceLabs({ user: SAUCELABS_USER, key: SAUCELABS_ACCESS_KEY });
--- features/support/world.js
+++ features/support/world.js
@@ -1,85 +1,17 @@
 /* eslint-disable func-names */
 const { setDefaultTimeout, setWorldConstructor } = require('cucumber');
-const { Builder } = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-const chrome = require('selenium-webdriver/chrome');
-const safari = require('selenium-webdriver/safari');
-
-const { SAUCELABS_ACCESS_KEY, SAUCELABS_HOST, SAUCELABS_USER } = process.env;
 
 setDefaultTimeout(60 * 1000);
 
-const capabilities = {
-    ie: {
-        browserName: 'internet explorer',
-        iedriverVersion: '3.14.0',
-        seleniumVersion: '3.14.0',
-        version: '11.285',
-    },
-    chrome: {
-        browserName: 'chrome',
-        screenResolution: '1920x1200',
-    },
-    firefox: {
-        browserName: 'firefox',
-    },
-    safari: {
-        browserName: 'safari',
-        screenResolution: '1920x1440',
-    },
-};
-
-const getBrowser = (browser) => () => new Builder()
-    .withCapabilities({
-        username: SAUCELABS_USER,
-        accessKey: SAUCELABS_ACCESS_KEY,
-        host: SAUCELABS_HOST,
-        port: 4445,
-        implicit: 5000,
-        ...capabilities[browser],
-    })
-    .usingServer(`http://${SAUCELABS_USER}:${SAUCELABS_ACCESS_KEY}@${SAUCELABS_HOST}:4445/wd/hub`)
-    .build();
-
-const getLocalBrowser = (browser, display) => {
-    let options = null;
-    let optionsFunc = null;
-
-    switch (browser) {
-        case 'chrome':
-            options = new chrome.Options();
-            optionsFunc = 'setChromeOptions';
-            break;
-        case 'safari':
-            options = new safari.Options();
-            optionsFunc = 'setSafariOptions';
-            break;
-        default:
-            options = new firefox.Options();
-            optionsFunc = 'setFirefoxOptions';
-            break;
-    }
-
-    if (!display) {
-        options.headless();
-    }
-
-    const builder = new Builder();
-
-    return builder[optionsFunc](options)
-        .forBrowser(browser)
-        .build();
-};
-
 const World = function World(args) {
-    const { browser } = args.parameters;
+    this.browser = args.parameters.browser;
+    this.local = args.parameters.local;
+    this.display = args.parameters.display;
 
     if (args.parameters.local) {
         this.host = 'http://localhost:3000';
-        this.driver = getLocalBrowser(browser, args.parameters.display);
     } else {
         this.host = 'http://app.local:3000';
-        this.driver = getBrowser(browser)();
     }
 };
 
