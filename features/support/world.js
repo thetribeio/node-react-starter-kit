d183fc43d6a8f79eda909c95bf29f2cc620e24d3
Jérôme <jerome.vasseur@thetribe.io>
2019-09-18 10:35:32 +0200

    Une an ES6 class for cucumber World

--- features/support/world.js
+++ features/support/world.js
@@ -1,20 +1,21 @@
-/* eslint-disable func-names */
 import { setDefaultTimeout, setWorldConstructor } from 'cucumber';
 import Factory from '../pages/Factory';
 
 setDefaultTimeout(60 * 1000);
 
-const World = function World(args) {
-    this.browser = args.parameters.browser;
-    this.local = args.parameters.local;
-    this.display = args.parameters.display;
-    this.pageFactory = new Factory(this);
+class World {
+    constructor(args) {
+        this.browser = args.parameters.browser;
+        this.local = args.parameters.local;
+        this.display = args.parameters.display;
+        this.pageFactory = new Factory(this);
 
-    if (args.parameters.local) {
-        this.host = 'http://localhost:3000';
-    } else {
-        this.host = 'http://app.local:3000';
+        if (args.parameters.local) {
+            this.host = 'http://localhost:3000';
+        } else {
+            this.host = 'http://app.local:3000';
+        }
     }
-};
+}
 
 setWorldConstructor(World);
