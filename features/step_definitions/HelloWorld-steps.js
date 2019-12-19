bf8a39983363a13c50797f9fdc2a3417e668b32f
Thomas Delcros <delcrosthomas@gmail.com>
2019-12-19 14:36:15 +0100

    Remove id mention in feature file

--- features/step_definitions/HelloWorld-steps.js
+++ features/step_definitions/HelloWorld-steps.js
@@ -1,11 +1,18 @@
 import { Given, Then, When } from 'cucumber';
 
+const contentToId = {
+    'You clicked 0 times': 'click-me',
+    'Translate in fr': 'translate',
+};
+
 Given('I am on Home page', async function () {
     this.page = this.pageFactory.create('Home');
     await this.page.loadAndWaitUntilVisible();
 });
 
-When('I press the {string} button', async function (id) {
+When('I press the {string} button', async function (content) {
+    const id = contentToId[content];
+
     await this.page.clickButtonById(id);
 });
 
